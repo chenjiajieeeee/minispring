865fcf8856a5b127d4e5019e9f075c7865bc1dad
chenjiajieeeee <2868203618@qq.com>
2024-02-04 18:03:55 +0800

    构建了资源以及资源加载器： 资源分三种：url资源、文件资源、类路径资源 加载器其实就是运用了设计模式种的简单工厂模式

---
+++ src/main/java/org/springframework/beans/core/io/DefaultResourceLoader.java
@@ -0,0 +1,30 @@
+package org.springframework.beans.core.io;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author Tina
+ * @date 2024-02-04
+ * 默认的资源加载器
+ */
+public class DefaultResourceLoader implements ResourceLoader{
+    public static final String CLASSPATH_URL_PREFIX = "classpath:";
+
+    @Override
+    public Resource getResource(String location) {
+        if (location.startsWith(CLASSPATH_URL_PREFIX)) {
+            //classpath下的资源
+            return new ClassPathResource(location.substring(CLASSPATH_URL_PREFIX.length()));
+        } else {
+            try {
+                //尝试当成url来处理
+                URL url = new URL(location);
+                return new UrlResource(url);
+            } catch (MalformedURLException ex) {
+                //当成文件系统下的资源处理
+                return new FileSystemResource(location);
+            }
+        }
+    }
+}
