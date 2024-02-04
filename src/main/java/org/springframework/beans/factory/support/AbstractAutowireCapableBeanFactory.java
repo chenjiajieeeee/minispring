d3a68961eaefae2e569fb4a095d909d5d0acfd14
chenjiajieeeee <2868203618@qq.com>
2024-02-04 15:07:52 +0800

    现在bean对象可以设置属性了，通过hutool的beanutil反射进行设置

--- src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1,10 +1,13 @@
 package org.springframework.beans.factory.support;
 
+import cn.hutool.core.bean.BeanUtil;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyValue;
 import org.springframework.beans.factory.config.BeanDefinition;
 
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory{
     private InstantiationStrategy instantiationStrategy = new SimpleInstantiationStrategy();
+
     @Override
     protected Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException {
         return doCreateBean(beanName, beanDefinition);
@@ -14,6 +17,8 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         Object bean = null;
         try {
             bean = createBeanInstance(beanDefinition);
+            //为bean填充属性
+            applyPropertyValues(beanName, bean, beanDefinition);
         } catch (Exception e) {
             throw new BeansException("Instantiation of bean failed", e);
         }
@@ -22,8 +27,34 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         return bean;
     }
 
+    /**
+     * 实例化bean
+     *
+     * @param beanDefinition
+     * @return
+     */
     protected Object createBeanInstance(BeanDefinition beanDefinition) {
         return getInstantiationStrategy().instantiate(beanDefinition);
+    }
+
+    /**
+     * 为bean填充属性
+     *
+     * @param bean
+     * @param beanDefinition
+     */
+    protected void applyPropertyValues(String beanName, Object bean, BeanDefinition beanDefinition) {
+        try {
+            for (PropertyValue propertyValue : beanDefinition.getPropertyValues().getPropertyValues()) {
+                String name = propertyValue.getName();
+                Object value = propertyValue.getValue();
+
+                //通过反射设置属性
+                BeanUtil.setFieldValue(bean, name, value);
+            }
+        } catch (Exception ex) {
+            throw new BeansException("Error setting property values for bean: " + beanName, ex);
+        }
     }
 
     public InstantiationStrategy getInstantiationStrategy() {
