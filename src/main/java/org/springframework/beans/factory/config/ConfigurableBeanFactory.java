0d9e39892b88acae6ba448377f83254066135529
chenjiajieeeee <2868203618@qq.com>
2024-02-07 16:54:25 +0800

    application实现自动化bean的属性修改

--- src/main/java/org/springframework/beans/factory/config/ConfigurableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/config/ConfigurableBeanFactory.java
@@ -3,6 +3,7 @@ package org.springframework.beans.factory.config;
 import org.springframework.beans.factory.HierarchicalBeanFactory;
 
 public interface ConfigurableBeanFactory extends HierarchicalBeanFactory, SingletonBeanRegistry {
+
     /**
      * @param beanPostProcessor
      */
--- src/main/java/org/springframework/beans/factory/support/CglibSubclassingInstantiationStrategy.java
+++ src/main/java/org/springframework/beans/factory/support/CglibSubclassingInstantiationStrategy.java
@@ -5,7 +5,8 @@ import net.sf.cglib.proxy.MethodInterceptor;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 
-public class CglibSubclassingInstantiationStrategy implements InstantiationStrategy{
+public class CglibSubclassingInstantiationStrategy implements InstantiationStrategy {
+
     /**
      * 使用CGLIB动态生成子类
      *
@@ -15,9 +16,7 @@ public class CglibSubclassingInstantiationStrategy implements InstantiationStrat
      */
     @Override
     public Object instantiate(BeanDefinition beanDefinition) throws BeansException {
-        Enhancer enhancer = new Enhancer();
-        enhancer.setSuperclass(beanDefinition.getBeanClass());
-        enhancer.setCallback((MethodInterceptor) (obj, method, argsTemp, proxy) -> proxy.invokeSuper(obj,argsTemp));
-        return enhancer.create();
+        //TODO 感兴趣的小伙伴可以实现下
+        throw new UnsupportedOperationException("CGLIB instantiation strategy is not supported");
     }
 }
